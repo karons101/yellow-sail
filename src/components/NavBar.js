45659aaa3014d6f6de91da2369e4f13b1e9e26ce
unknown <karonstical@gmail.com>
2025-01-26 22:14:16 -0800

    Added back the videos

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAnchor, faTimes } from '@fortawesome/free-solid-svg-icons'; // Import Font Awesome icons
-import './NavBar.css'; // Import the CSS file for styling
+import './Navbar.css'; // Import the CSS file for styling
 
 const Navbar = () => {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
